00e3be661d3152da5c759fc577c195555fd717bc
Andrew Gurik <gurellia53@gmail.com>
2023-12-22 13:00:42 -0600

    switch to CommandXboxController

--- neutrino_sim/src/main/java/frc/robot/RobotContainer.java
+++ neutrino_sim/src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,9 @@ package frc.robot;
 
 import com.revrobotics.REVPhysicsSim;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.DriveCommand;
 import frc.robot.commands.WheelStop;
 import frc.robot.commands.WheelSpin;
@@ -18,16 +17,12 @@ import frc.robot.subsystems.Wheel;
 
 public class RobotContainer {
 
-    private XboxController m_controller;
+    private CommandXboxController m_controller;
     private Drivetrain m_drivetrain;
     private Wheel m_wheel;
-    private JoystickButton m_A;
-    private JoystickButton m_B;
 
     public RobotContainer() {
-        m_controller = new XboxController(0);
-        m_A = new JoystickButton(m_controller, XboxController.Button.kA.value);
-        m_B = new JoystickButton(m_controller, XboxController.Button.kB.value);
+        m_controller = new CommandXboxController(0);
         m_drivetrain = new Drivetrain();
         m_wheel = new Wheel();
 
@@ -38,8 +33,8 @@ public class RobotContainer {
         m_drivetrain.setDefaultCommand(new DriveCommand(m_drivetrain, m_controller));
         m_wheel.setDefaultCommand(new WheelStop(m_wheel));
 
-        m_A.whileTrue(new WheelSpin(m_wheel, 60));
-        m_B.whileTrue(new WheelSpin(m_wheel, 120));
+        m_controller.a().whileTrue(new WheelSpin(m_wheel, 60));
+        m_controller.b().whileTrue(new WheelSpin(m_wheel, 120));
     }
 
     public Command getAutonomousCommand() {
--- neutrino_sim/src/main/java/frc/robot/commands/DriveCommand.java
+++ neutrino_sim/src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,14 +1,14 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class DriveCommand extends Command {
     DrivetrainSubsystem m_drivetrain;
-    XboxController m_controller;
+    CommandXboxController m_controller;
 
-    public DriveCommand(DrivetrainSubsystem drivetrain, XboxController controller) {
+    public DriveCommand(DrivetrainSubsystem drivetrain, CommandXboxController controller) {
         m_drivetrain = drivetrain;
         m_controller = controller;
         addRequirements(m_drivetrain);
